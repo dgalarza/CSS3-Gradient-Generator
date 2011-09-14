f4e96c86583e2d7247f38bc918b9bdcece465c4f
Damian Galarza <galarza.d@gmail.com>
2011-09-13 23:32:46 -0400

    Upcoming updates to the css gradient generator

---
+++ generator2.js
@@ -0,0 +1,25 @@
+/**
+ * CSS3 Gradient Generator
+ *
+ * css3factory.com
+ * Damian Galarza
+ */
+(function() {
+
+  var Swatch = function() {
+
+  };
+
+  Swatch.prototype = {
+
+  };
+
+  var generator = {
+
+  };
+
+  $(function() {
+
+  });
+
+})();
